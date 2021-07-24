cb6120a3a831be8c9b8e1291e0e26055f3da4da1
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-23 22:20:33 -0700

    Working on skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Intro from './components/Intro';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Skills from './components/Skills';
 
 function App() {
+  const [introPos, setIntroPos] = useState([0,0])
+  const [scrollPos, setScrollPos] = useState(0)
+
+  useEffect(()=>{
+    document.addEventListener('scroll',e=>{
+      let scrolled = document.scrollingElement.scrollTop;
+      setScrollPos(scrolled)
+    })
+  },[])
+
   return (
-    <div >
-      
+    <div id='app'>
+      <Intro setIntroPos={setIntroPos} introPos={introPos} scrollPos={scrollPos} />
+      <About />
+      <Skills />
+      <Projects />
+      <Resume />
+      <Contact />
+      <button onTouch={()=>window.scroll(0,introPos)} onClick={()=>window.scroll(0,introPos)}>SetScrollPos to 0</button>
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
-const About =()=>{
-    return(
-        <div>
-
-        </div>
+import './About.css'
+const About = () => {
+    return (
+        <section id='about'>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iure cupiditate numquam voluptates perferendis libero maiores officia consectetur minima id tenetur, animi, quae sapiente ipsa vel accusamus, aperiam ut aliquid?</p>
+        </section>
     )
 }
 export default About
