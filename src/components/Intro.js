f3a14efc5c516ec8a0de81c75748ecfe947cc9ea
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-27 11:11:27 -0700

    Working on styling.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,13 +4,14 @@ const Intro =(props)=>{
     const {setIntroPos, introPos, scrollPos} = props;
 
     return (
-        <section style={{"opacity":`${1 - scrollPos/250}`}} id='intro' ref={el=>{
+        <section id='intro' ref={el=>{
             if(!el) return;
             setIntroPos(el.getBoundingClientRect().y)
         }}>
-            <img style={{"height": `${1 - scrollPos/250 <= 0 ? "0" : "105vh"}`}} className={`int-back`} id='intro-background' src='https://media-exp1.licdn.com/dms/image/C561BAQGEbvT3SFyR9Q/company-background_10000/0/1582050035728?e=2159024400&v=beta&t=xwPLRsVBBNXQQS3HN3q7hsYXmt6JxJsH6lpnbh9Y1ko' />
-            <img id="profile-pic" src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='Bret Fernberg with a rather dashing haircut, smiling at the sun'/>
-            <h1 >Hello, my name is <span id='my-name'>Bret Fernberg</span>, and I'm a Full Stack Web Developer. It's a pleasure to meet you!</h1>
+            <div id='profile-wrapper'>
+                <img id="profile-pic" src='https://storage.googleapis.com/cool-bucket-wow/profilepic.jpg' alt='Bret Fernberg with a rather dashing haircut, smiling at the sun'/>
+                <p id='intro-p'>Hello, my name is <h1 id='my-name'>Bret Fernberg</h1>and I'm a Full Stack Web Developer. It's a pleasure to meet you!</p>
+            </div>
         </section>
     )
 }
