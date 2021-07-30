8b006eee5a48f02cea430493817c31f9c44e9991
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-29 20:31:30 -0700

    More styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,7 +12,9 @@ const Intro =(props)=>{
             <div className='container pic-filter int-back-filt'></div>
             <div id='profile-wrapper'>
                 <img id="profile-pic" src='https://storage.googleapis.com/cool-bucket-wow/profilepic.jpg' alt='Bret Fernberg with a rather dashing haircut, smiling at the sun'/>
-                <p id='intro-p'>Hello, my name is <h1 id='my-name'>Bret Fernberg</h1>and I'm a Full Stack Web Developer. It's a pleasure to meet you!</p>
+                <p className='intro-p'>Hello, my name is</p>
+                <h1 id='my-name'>Bret Fernberg</h1> 
+                <p className='intro-p'>and I'm a Full Stack Web Developer. It's a pleasure to meet you!</p>
             </div>
         </section>
     )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ const Resume =()=>{
     return(
         <section className='card container' id='resume-container'>
             <h2>RESUME</h2>
-            <iframe view='fit' style={{"min-height":"100vh","width":"100%"}} src="https://drive.google.com/file/d/15Qn0R6WfKydVCCCmEH2-wcM2SKxns2js/preview#view=fitH" allow="autoplay"></iframe>
+            <iframe view='fit' src="https://drive.google.com/file/d/15Qn0R6WfKydVCCCmEH2-wcM2SKxns2js/preview" allow="autoplay"></iframe>
         </section>
     )
 }
