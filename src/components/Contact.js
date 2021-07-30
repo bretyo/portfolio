c989cc5815f139f527fe72c08c4526c2ceba0c3e
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-29 22:04:17 -0700

    Styled the contact component, solved the resume, widened the text space in the intro

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
+import './Contact.css'
 const Contact =()=>{
     return(
         <section className='card container' id='contact-links'>
             <h2>CONTACT ME</h2>
-            <a href='https://www.linkedin.com/in/bret-fernberg/'>LinkedIn</a>
-            <a href='https://github.com/bretyo'>Github</a>
-            <h3>Email: bret.fernberg@gmail.com</h3>
-            <h3>Phone Number: 801-930-0218</h3>
+            <h3 className='contact-name'>LinkedIn: <a className='contact' href='https://www.linkedin.com/in/bret-fernberg'>https://www.linkedin.com/in/bret-fernberg</a></h3>
+            <h3 className='contact-name'>Github: <a className='contact' href='https://github.com/bretyo'>https://github.com/bretyo</a></h3>
+            <h3 className='contact-name'>Email: <span className='contact'>bret.fernberg@gmail.com</span></h3>
+            <h3 className='contact-name'>Phone Number: <span className='contact'>801-930-0218</span></h3>
         </section>
     )
 }
