c12d345b8f798cfbf48d2623f6eaa0bb27404c00
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-30 15:31:17 -0700

    styled the contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,10 @@ const Contact =()=>{
     return(
         <section className='card container' id='contact-links'>
             <h2>CONTACT ME</h2>
-            <h3 className='contact-name'>LinkedIn: <a className='contact' href='https://www.linkedin.com/in/bret-fernberg'>https://www.linkedin.com/in/bret-fernberg</a></h3>
-            <h3 className='contact-name'>Github: <a className='contact' href='https://github.com/bretyo'>https://github.com/bretyo</a></h3>
-            <h3 className='contact-name'>Email: <span className='contact'>bret.fernberg@gmail.com</span></h3>
-            <h3 className='contact-name'>Phone Number: <span className='contact'>801-930-0218</span></h3>
+            <h3 className='contact'><span className='contact-title'>LinkedIn: </span><a className='contact-info' href='https://www.linkedin.com/in/bret-fernberg'>https://www.linkedin.com/in/bret-fernberg</a></h3>
+            <h3 className='contact'><span className='contact-title'>Github: </span><a className='contact-info' href='https://github.com/bretyo'>https://github.com/bretyo</a></h3>
+            <h3 className='contact'><span className='contact-title'>Email: </span><span className='contact-info'>bret.fernberg@gmail.com</span></h3>
+            <h3 className='contact'><span className='contact-title'>Phone Number: </span><span className='contact-info'>801-930-0218</span></h3>
         </section>
     )
 }
