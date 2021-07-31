4910b47735cc2ecda9582c4bddfc1a542ad0a2d2
Bret Fernberg <bret.fernberg@gmail.com>
2021-07-30 17:02:47 -0700

    General styling, also had to fix the underlines in chrome

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ const About = () => {
         <section className='container card' id='about'>
             <h2>ABOUT ME</h2>
             <div className='abt-pic-txt-container'>
-                <img className='abt-pic' src={'https://thumbs.dreamstime.com/b/happy-family-summer-vacation-people-having-fun-beach-active-healthy-lifestyle-family-summer-vacation-beach-sea-140887794.jpg'} />
-                <p className='abt-txt'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iure cupiditate numquam voluptates perferendis libero maiores officia consectetur minima id tenetur, animi, quae sapiente ipsa vel accusamus, aperiam ut aliquid?</p>
+                <img className='abt-pic' src={'https://d.newsweek.com/en/full/1533980/devmountain-logo-lockup-blue.png?w=560&f=71b1cf646452985803eb1b7e185c6b9f'} />
+                <p className='abt-txt'>After nearly 5 years of working in the food service industry, I decided to take a chance and join the tech industry! I enrolled in the Full-Time Immersive Web Development program at Devmountain in Lehu, UT, where I learnt the skills required to be a Full Stack Web Developer.</p>
             </div>
             <div className='abt-pic-txt-container'>
                 <p className='abt-txt'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iure cupiditate numquam voluptates perferendis libero maiores officia consectetur minima id tenetur, animi, quae sapiente ipsa vel accusamus, aperiam ut aliquid?</p>
