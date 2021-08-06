abd2bf90c934684d161605a04e0cf35118fc5d0b
Bret Fernberg <bret.fernberg@gmail.com>
2021-08-06 14:49:06 -0600

    Added a link to resume, as well as updated the icon

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,11 @@
 import './Resume.css'
-const Resume =()=>{
-    return(
+const Resume = () => {
+    return (
         <section data-aos='zoom-in' className='card container' id='resume-container'>
-            <h2>RESUME</h2>
+            <a id='resume-link' href='https://drive.google.com/file/d/15Qn0R6WfKydVCCCmEH2-wcM2SKxns2js/view'>
+                <h2 >RESUME</h2>
+                <aside>click here for link</aside>
+            </a>
             <iframe view='fit' src="https://drive.google.com/file/d/15Qn0R6WfKydVCCCmEH2-wcM2SKxns2js/preview" allow="autoplay"></iframe>
         </section>
     )
